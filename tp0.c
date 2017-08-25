9663e914d101253a6d1cd8cbf40db974e1cb21ab
matias <feldmatias@gmail.com>
2017-08-25 11:55:32 -0300

    agregado errores de apertura de archivos y -h y -v
    creo que no falta nada, solo el informe y agregar mas pruebas si quieren

--- tp0.c
+++ tp0.c
@@ -45,12 +45,44 @@ bool es_capicua(char* palabra, int len){
 int main(int argc, char* argv[]){
 	FILE* entrada = stdin;
 	FILE* salida = stdout;
+	char* parametro;
+	
 	for (int i = 1; i < argc; i += 2){
 		if (strcmp(argv[i],"-i") == 0){
-			entrada = fopen(argv[i + 1], "r");
+			if (i + 1 >= argc){
+				fputs("Debe indicar un archivo de entrada luego de -i\n", stderr);
+				return 2;
+			}
+			parametro = argv[i + 1];
+			if (strcmp(parametro,"-") != 0){
+				entrada = fopen(argv[i + 1], "r");
+				if (!entrada){
+					fputs("El archivo de entrada no pudo abrirse\n", stderr);
+					return 1;
+				}
+			}
 		}
 		else if (strcmp(argv[i],"-o") == 0){
-			salida = fopen(argv[i + 1], "w");
+			if (i + 1 >= argc){
+				fputs("Debe indicar un archivo de salida luego de -o\n", stderr);
+				return 2;
+			}
+			parametro = argv[i + 1];
+			if (strcmp(parametro,"-") != 0){
+				salida = fopen(argv[i + 1], "w");
+				if (!salida){
+					fputs("El archivo de salida no pudo abrirse\n", stderr);
+					return 1;
+				}
+			}
+		}
+		else if (strcmp(argv[i],"-V") == 0){
+			fprintf(stdout, "TP0 version 1.0001\n");
+			return 0;
+		}
+		else if (strcmp(argv[i],"-h") == 0){
+			fprintf(stdout, "Usage:\n\ntp0 -h\ntp0 -V\ntp0 [options]\n\nOptions:\n-V, --version  Print version and quit.\n-h, --help   Print this information.\n-i, --input   Location of the input file.\n-o, --output   Location of the output file.\n\nExample:\ntp0 -i ~/input -o ~/output\n");
+			return 0;
 		}
 	}
 	
@@ -63,7 +95,9 @@ int main(int argc, char* argv[]){
 		}
 		free (palabra);
 	}
+	
 	fclose(entrada);
 	fclose(salida);
+	
 	return 0;
 }
