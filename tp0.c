94a8c19d3933c7a6973e42cf8c96e770b47331f0
matias <feldmatias@gmail.com>
2017-08-25 10:36:05 -0300

    probado funcionando en linux

--- tp0.c
+++ tp0.c
@@ -1,31 +1,37 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-#DEFINE TAM 10
+#include <stdlib.h>
+#include <ctype.h>
+#define TAM 10
 
 
-char * leer_palabra(FILE* archivo){
+char * leer_palabra(FILE* archivo, int* longitud){
 	char* palabra = realloc(NULL,TAM);
-	int i = 0;
+	int len = 0;
 	while(true){
-		int caracter = fgetc(archivo);
-		if(){// agregar la comparacion
-			palabra[i] = caracter;
-			i++;
-			if (i%TAM == 0){
-				realloc(palabra, TAM + i);
+		int c = fgetc(archivo);
+		if((c>=65 && c<=90) || (c>=97 && c<=122) || (c == 95) || (c == 45)){
+			palabra[len] = c;
+			len ++;
+			if (len % TAM == 0){
+				palabra = realloc(palabra, TAM + len);
 			}
 		}
 		else{
-			palabra[i] = '\0';
+			palabra[len] = '\0';
+			*longitud = len;
 			return palabra;
 		}
 	}
 }
 
-bool es_capicua(char* palabra){
+bool es_capicua(char* palabra, int len){
+	if (len == 0){
+		return false;
+	}
 	int inicio = 0;
-	int final = strlen(palabra) - 1;
+	int final = len - 1;
 	while(inicio < final){
 		if (tolower(palabra[inicio]) != tolower(palabra[final])){
 			return false;
@@ -41,16 +47,17 @@ int main(int argc, char* argv[]){
 	FILE* salida = stdout;
 	for (int i = 1; i < argc; i += 2){
 		if (strcmp(argv[i],"-i") == 0){
-			entrada = fopen(argv[i + 1], 'r');
+			entrada = fopen(argv[i + 1], "r");
 		}
 		else if (strcmp(argv[i],"-o") == 0){
-			salida = fopen(argv[i + 1], 'w');
+			salida = fopen(argv[i + 1], "w");
 		}
 	}
-	while(EOF(entrada)){// buscar la funcion
-		char* palabra = leer_palabra(entrada);
-		if (es_capicua(palabra)){
-			fprintf(salida, '%s\n', palabra);
+	while(!feof(entrada)){
+		int len;
+		char* palabra = leer_palabra(entrada, &len);
+		if (es_capicua(palabra, len)){
+			fprintf(salida, "%s\n", palabra);
 		}
 		free (palabra);
 	}
